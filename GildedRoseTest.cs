79bb5a7de97071973b6eb1c0f4ba00deafa75d9d
Matteo Pierro <pierromatteo@gmail.com>
2019-01-28 17:48:12 +0100

    inital commit

---
+++ GildedRoseTest.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using ApprovalTests.Reporters;
+using ApprovalTests;
+using System;
+
+namespace csharpcore
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("fixme", Items[0].Name);
+        }
+
+        [Fact]
+        public void TestUseOfApprovalTests()
+        {
+            var names = new[] { "Llewellyn", "James", "Dan", "Jason", "Katrina" };
+            Array.Sort(names);
+            Approvals.VerifyAll(names, "");
+        }
+    }
+}

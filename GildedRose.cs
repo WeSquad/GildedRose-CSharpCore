e39979cdc28fe60313dd7b2336c062b5d32aabca
sewelseb <sewelseb@hotmail.com>
2019-02-04 14:02:01 +0100

    Sulfuras, Hand of Ragnaros

--- GildedRose.cs
+++ GildedRose.cs
@@ -37,68 +37,54 @@ namespace csharpcore
                     }
                 }
             }
-            else
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality < 50)
                 {
-                    if (item.Quality > 0)
+                    item.Quality = item.Quality + 1;
+
+                    if (item.SellIn < 11)
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        if (item.Quality < 50)
                         {
-                            item.Quality = item.Quality - 1;
+                            item.Quality = item.Quality + 1;
                         }
                     }
-                }
-                else
-                {
-                    if (item.Quality < 50)
+
+                    if (item.SellIn < 6)
                     {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality < 50)
                         {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
+                            item.Quality = item.Quality + 1;
                         }
                     }
                 }
 
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+                item.SellIn = item.SellIn - 1;
 
                 if (item.SellIn < 0)
                 {
+                    item.Quality = item.Quality - item.Quality;
+                }
+            }
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+            }
+            else
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
+                }
 
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
+                item.SellIn = item.SellIn - 1;
 
+                if (item.SellIn < 0)
+                {
+                    if (item.Quality > 0)
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
                 }
             }
         }
